e48121ccb26e6b06e161f3765457bef1b3b336ad
Jerome-bouvier <moeb.bzhash@gmail.com>
2018-05-22 12:31:06 +0200

    critical availability tests on hp

--- cypress/integration/go_spec.js
+++ cypress/integration/go_spec.js
@@ -4,5 +4,11 @@ describe('go home', function () {
 
         cy.visit('https://jeromebouvier.fr/')
         cy.get('.main-logo').should('be.visible')
+        cy.get('.search-icon').should('be.visible')
+        cy.get('.menu-bar').should('be.visible').click()
+        cy.get('nav').should('be.visible')
+        cy.get('h1').contains('./Photographies')
+        cy.get('.footer-bottom-content').scrollIntoView()
+        cy.get('.flaticon-bottom4').click()
     })
 })
